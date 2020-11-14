8a3acf2b9081c38c3cafc5b7398a029ab71e946b
keisuke <keisuke.yamauchi.321@gmail.com>
2020-11-14 16:06:15 +0900

    update 入力を促してる感を出す

--- input/input.go
+++ input/input.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CreateUrl() string {
-	fmt.Print("Please enter the name of the Pokemon.\n")
+	fmt.Print("Please enter the name of the Pokemon.\n>> ")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return "http://localhost:8081/?name=" + scanner.Text()
